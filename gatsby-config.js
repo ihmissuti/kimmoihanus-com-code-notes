832a20a9d0ab6e2f9e364c4ed8b3b4a9fe186835
ihmissuti <ihmissuti@gmail.com>
2020-11-04 09:08:54 +0200

    manifest.json update

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,21 +27,21 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Kimmo's Code Notes`,
-        short_name: `CodeNotesByKimmo`,
-        description: `Notes on code. By Kimmo Ihanus.`,
-        start_url: `/`,
-        background_color: `hsl(210, 38%, 95%)`,
-        theme_color: `hsl(345, 100%, 69%)`,
-        display: `standalone`,
+        // name: `Kimmo's Code Notes`,
+        // short_name: `CodeNotesByKimmo`,
+        // description: `Notes on code. By Kimmo Ihanus.`,
+        // start_url: `/`,
+        // background_color: `hsl(210, 38%, 95%)`,
+        // theme_color: `hsl(345, 100%, 69%)`,
+        // display: `standalone`,
         icon: `public/static/favicon-32x32.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/*`, `/tag/*`],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     precachePages: [`/*`, `/tag/*`],
+    //   },
+    // },
   ],
 }
